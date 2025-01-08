09c88a2b35e8bf4798c84e30f0602e41ecc7d6f7
hafsa-saeed <hafsasaeed1074@gmail.com>
2025-01-08 14:44:58 -0800

    initial comment for portfolio project

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('open');
+}
+// function toggleMenu() {
+//   const sidebar = document.getElementById('sidebar');
+//   if(sidebar.classList.toggle('open')){
+//     sidebaridebar.classList.toggle('closed');
+//   }else{
+//     if(idebar.classList.toggle('closed')){
+//       sidebar.classList.toggle('open');
+//     }
+//   }
+// }
